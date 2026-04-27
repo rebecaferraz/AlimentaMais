e7f41c42802785a71d70510a4459dcb466a16bbd
Rbekah <rferr4zz@gmail.com>
2026-04-27 11:39:56 -0300

    Fix startup.sh to activate Azure virtualenv before running gunicorn

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
-pip install -r requirements.txt
-python manage.py migrate
-gunicorn --bind=0.0.0.0 --timeout 600 config.wsgi
+#!/bin/bash
+source antenv/bin/activate
+python manage.py migrate --noinput
+gunicorn --bind=0.0.0.0:8000 --timeout 600 config.wsgi
